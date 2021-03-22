f38d4aa2fd254f8f9c9b58a3b2c58aaeec5949de
Jean Lobos <jeanavlobos13@gmail.com>
2021-03-22 19:23:30 -0300

    Primeiro commit: Projeto Criado

---
+++ src/main/java/com/jeanlobos/provadenox1/Provadenox1Application.java
@@ -0,0 +1,13 @@
+package com.jeanlobos.provadenox1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provadenox1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Provadenox1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeanlobos/provadenox1/Provadenox1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlobos.provadenox1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provadenox1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
